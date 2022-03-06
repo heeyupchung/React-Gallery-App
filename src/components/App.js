bc1a47f997b2e7e639a5ae4efdb1af8ff7211e4b
heeyupchung <heeyupchung@gmail.com>
2022-03-05 17:25:46 -0800

    update default path

--- src/components/App.js
+++ src/components/App.js
@@ -57,7 +57,7 @@ class App extends Component {
         <Nav />
         { (this.state.loading) ? <Loading /> :
             <Switch>
-              <Route path='/fleeker'><Default /></Route>
+              <Route path={process.env.PUBLIC_URL + '/'}><Default /></Route>
               <Route path='/search/:query' render={() => <PhotoContainer data={this.state.photos} title={this.state.title} /> } />
               <Route path='/cats' render={() => <PhotoContainer data={cats} title='cats' /> } />
               <Route path='/dogs' render={() => <PhotoContainer data={dogs} title='dogs' /> } />
